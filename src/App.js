e964927ad36c58d421ba4605bb5c21a02da43d35
nbos17 <nbos0531@gmail.com>
2018-09-29 17:10:04 -0500

    starting

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p>Sell Your Mac</p>
+        <div className="container-fluid">
+
+          <div className='row'>
+            <div className="col-12">
+              <p>Sell Your Mac</p>
+            </div>
+          </div>
+
+          <div className='row'>
+            <div class="col-12" id="nav">
+              <ul>
+                <li>Macbook Pro</li>
+                <li>Macbook Air</li>
+                <li>Macbook</li>
+                <li>iMac</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
       </div>
     );
   }
